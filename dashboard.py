74a9d5646286e6a512bad4864a53e9c185498bc1
JopRijks <31651172+JopRijks@users.noreply.github.com>
2018-06-19 16:45:13 +0200

    Stacked Bar toegevoegd

--- dashboard.py
+++ dashboard.py
@@ -10,6 +10,7 @@ app = dash.Dash()
 
 df = pd.read_csv('globalterrorismdb_0616dist.csv', encoding = "cp1252")
 dataternat = terroristnationality("Afghanistan", df)
+predata = attacktype(df, "Afghanistan")
 
 app.layout = html.Div(children=[
     html.H1(children='Informatie Visualisatie'),
@@ -17,10 +18,9 @@ app.layout = html.Div(children=[
     html.Div(children='''
         Groep A5 met Jop Rijksbaron, Milan Doodeman, Julien Coudron en Mark Muller.
     '''),
-    html.Label('Text Input'),
-    dcc.Input(id='country', value='country', type='text'),
+
     dcc.Graph(
-        id='Nationality of terrorists in the given country',
+        id='terrorist nationality',
         figure={
             'data': [
                 go.Pie(
@@ -32,7 +32,27 @@ app.layout = html.Div(children=[
                 'title': "Nationality of terrorists in the given country"
             }
         }
-    )
+    ),
+    dcc.Graph(
+        id='kills against type of attack',
+        figure={
+            'data': [
+                {'x': predata[0], 'y': predata[1][0], 'type': 'bar', 'name': predata[2][0]},
+                {'x': predata[0], 'y': predata[1][1], 'type': 'bar', 'name': predata[2][1]},
+                {'x': predata[0], 'y': predata[1][2], 'type': 'bar', 'name': predata[2][2]},
+                {'x': predata[0], 'y': predata[1][3], 'type': 'bar', 'name': predata[2][3]},
+                {'x': predata[0], 'y': predata[1][4], 'type': 'bar', 'name': predata[2][4]},
+                {'x': predata[0], 'y': predata[1][5], 'type': 'bar', 'name': predata[2][5]},
+                {'x': predata[0], 'y': predata[1][6], 'type': 'bar', 'name': predata[2][6]},
+                {'x': predata[0], 'y': predata[1][7], 'type': 'bar', 'name': predata[2][7]},
+                {'x': predata[0], 'y': predata[1][8], 'type': 'bar', 'name': predata[2][8]},
+                
+            ],
+            'layout': {
+                'title': 'Attack Type through the years in kills per attack type',
+                'barmode': "stack"
+                }
+          })
 ])
 
 if __name__ == '__main__':
--- datafuncties.py
+++ datafuncties.py
@@ -21,3 +21,20 @@ def terroristnationality(country, df):
         nationality.append("Other")
         times.append(other)
     return nationality, times
+
+def attacktype(df, country):
+    everything = []
+    types = []
+    for type_attack in df["attacktype1_txt"].unique():
+        years = []
+        values = []
+        for year in df["iyear"].unique():
+            yearcheck = df[df["iyear"] == year]
+            countrycheck = yearcheck[df["country_txt"] == country]
+            attackcheck = countrycheck[df["attacktype1_txt"] == type_attack]
+            killcount = attackcheck["nkill"].sum()
+            years.append(year)
+            values.append(killcount)
+        everything.append(values)
+        types.append(type_attack)
+    return years, everything, types 
