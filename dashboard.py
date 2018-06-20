c2aa2ef17b88aec19b83fbf6c2c8cba6db50bc8c
JopRijks <31651172+JopRijks@users.noreply.github.com>
2018-06-20 11:37:04 +0200

    Doet het motherfockers

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 from datafuncties import *
@@ -9,30 +10,28 @@ from datafuncties import *
 app = dash.Dash()
 
 df = pd.read_csv('globalterrorismdb_0616dist.csv', encoding = "cp1252")
-dataternat = terroristnationality("Afghanistan", df)
-predata = attacktype(df, "Afghanistan")
 
-app.layout = html.Div(children=[
+# predata = attacktype(df, "Afghanistan")
+
+app.layout = html.Div([
     html.H1(children='Informatie Visualisatie'),
 
     html.Div(children='''
         Groep A5 met Jop Rijksbaron, Milan Doodeman, Julien Coudron en Mark Muller.
     '''),
 
-    html.Div(dcc.Input(id='input-box', type='text')),
-    html.Button('Submit', id='button'),
-    html.Div(id='output-container-button',
-             children='Enter a value and press submit'),
+    dcc.Input(id='input-box1', value="Afghanistan", type='text'),
+    dcc.Graph(id='output1'),
+])
 
-    dcc.Input(
-    placeholder='Enter a value...',
-    type='text',
-    value=''
-    ),
 
-    dcc.Graph(
-        id='terrorist nationality',
-        figure={
+@app.callback(
+    Output('output1', 'figure'),
+    [Input('input-box1', 'value')]
+)
+def update_output_div(input_value):
+    dataternat = terroristnationality(str(input_value), df)
+    return {
             'data': [
                 go.Pie(
                     labels=dataternat[0],
@@ -40,31 +39,9 @@ app.layout = html.Div(children=[
                 )
             ],
             "layout": {
-                'title': "Nationality of terrorists in the given country"
+                'title': "Nationality of terrorists in {}" .format(input_value)
             }
         }
-    ),
-    dcc.Graph(
-        id='kills against type of attack',
-        figure={
-            'data': [
-                {'x': predata[0], 'y': predata[1][0], 'type': 'bar', 'name': predata[2][0]},
-                {'x': predata[0], 'y': predata[1][1], 'type': 'bar', 'name': predata[2][1]},
-                {'x': predata[0], 'y': predata[1][2], 'type': 'bar', 'name': predata[2][2]},
-                {'x': predata[0], 'y': predata[1][3], 'type': 'bar', 'name': predata[2][3]},
-                {'x': predata[0], 'y': predata[1][4], 'type': 'bar', 'name': predata[2][4]},
-                {'x': predata[0], 'y': predata[1][5], 'type': 'bar', 'name': predata[2][5]},
-                {'x': predata[0], 'y': predata[1][6], 'type': 'bar', 'name': predata[2][6]},
-                {'x': predata[0], 'y': predata[1][7], 'type': 'bar', 'name': predata[2][7]},
-                {'x': predata[0], 'y': predata[1][8], 'type': 'bar', 'name': predata[2][8]},
-                
-            ],
-            'layout': {
-                'title': 'Attack Type through the years in kills per attack type',
-                'barmode': "stack"
-                }
-          })
-])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
